9dbcfe6a65a1aaf9b7d689a135e45b698cbc89a5
manishgupta577 <67793046+manishgupta577@users.noreply.github.com>
2021-08-04 15:03:52 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,483 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "MANISH",
+  logo_name: "manish.k()",
+  nickname: "manish / picleric",
+  full_name: "Manish Gupta",
+  subTitle:
+    "Full Stack Developer, Open Source Enthusiast 🔥. Always learning.",
+  resumeLink:
+    "https://docs.google.com/document/d/1A_jWRG74Rst427tg1izLa6vRXclS9_9F856jWJPJlDY/edit?usp=sharing",
+  mail: "mailto:gmanish577@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/manishgupta577",
+  linkedin: "https://www.linkedin.com/in/manish-gupta-5a299b1b3/",
+  gmail: "gmanish@gmail.com",
+
+  facebook: "https://www.facebook.com/profile.php?id=100003999962165",
+  twitter: "https://twitter.com/manish5773",
+  instagram: "https://www.instagram.com/gmanish577/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+        "⚡ Building responsive website front end using ReactJS",
+        
+        "⚡ Creating application backend in Node, Express & Mongodb",
+        "⚡ Integration of third party services such as AWS /Cpanel"
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "Express",
+          fontAwesomeClassname: "simple-icons:express",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        
+       
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+        
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+       
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+       
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Malviya national institute of technology",
+      subtitle: "Electronics and Communication enginerring",
+      logo_path: "ssgandhy.png",
+      alt_name: "SETI",
+      duration: "2018 - Present",
+      descriptions: [
+        "⚡ I have studied core subjects like Data Structures, DBMS",
+        "⚡ I have also completed various online courses for Backend , Web  etc.",
+        
+      ],
+      website_link: "http://mnit.ac.in/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "M0001: MongoDB Basics",
+      subtitle: "MongoDB University",
+      logo_path: "mongo.png",
+      
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "#47A048",
+    },
+    
+    {
+      title: "A300: Atlas Security",
+      subtitle: "MongoDB University",
+      logo_path: "mongo.png",
+     
+      alt_name: "MongoDB University",
+      // color_code: "#F6B808",
+      color_code: "#47A048",
+    },
+    
+   
+    
+    {
+      title: "Postman Student Expert",
+      subtitle: "Postman",
+      logo_path: "postman.png",
+     
+      alt_name: "Postman",
+      // color_code: "#f36c3d",
+      color_code: "#fffbf3",
+    },
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've completed two internship. I've mostly done projects on my own and now I am actively looking for placement.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "Full Stack Developer (Intern)",
+          company: "Kaizenglobe",
+          company_url: "https://kaizenglobe.com/",
+          logo_path: "kaizenglobe-logo.png",
+         
+          location: "Work from home",
+          description:
+            "I worked on different websites fixing their bugs and also created many websites for the company.Most of them were hosted on cpanel ,aws etc.",
+          color: "#0071C5",
+        },
+        {
+          title: "Full Stack Developer(Intern)",
+          company: "Exposys Data Labs",
+          company_url: "http://www.exposysdata.com/",
+          logo_path: "expo.png",
+          
+          location: "Work From Home",
+          description:
+            "Created Front end of Yearn Financial Mutual Funds website. also degined simple web application for better user experience, designed DB Schemas as well.",
+          color: "#ee3c26",
+        }
+      ],
+    },
+    
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, and React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "mani.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Thankyou",
+    subtitle:
+    "thank you for going through my portfolio ,please give a star to my github.",
+    link: "https://github.com/manishgupta577",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Chat room",
+      url: "https://let-s-chat-1.herokuapp.com/",
+      description: "this is a chatroom app created using nodejs,express and socketio",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "nodejs",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "express",
+          iconifyClass: "logos-express",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "Portfolio",
+      url: "https://portfolio577.herokuapp.com/",
+      description:
+        "My another portfolio website but using html,css and vanilla javascript. ",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "javascript",
+          iconifyClass: "logos-javascript",
+        },
+        
+      ],
+    },
+    {
+      id: "2",
+      name: "Mangotree",
+      url: "https://mangotreeltd.com/",
+      description:
+        "A website developed for business purposes by me during my internship",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "javascript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "Technology Portage",
+      url: "https://www.technologyportage.com/",
+      description:
+        " another website developed during my internship.",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+       
+      ],
+    },
+    {
+      id: "4",
+      name: "personal-portfolio",
+      url: "https://github.com/harikanani/personal-portfolio",
+      description:
+        "this spresent portfolio site which you are on ..",
+        languages: [
+          {
+            name: "JavaScript",
+            iconifyClass: "logos-javascript",
+          },
+          {
+            name: "ReactJS",
+            iconifyClass: "logos-react",
+          },
+          {
+            name: "HTML5",
+            iconifyClass: "logos-html-5",
+          },
+          {
+            name: "CSS3",
+            iconifyClass: "logos-css-3",
+          },
+        ],
+    },
+    {
+      id: "3",
+      name: "Weather app",
+      url: "https://weather-app577.herokuapp.com/",
+      description:
+        "Simple NodeJS Express CRUD Operations API starter.",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "Shopping website",
+      url: "http://rashitechnology.co.in/oeema_website/",
+      description:
+        "shopping website using wordpress ",
+      languages: [
+        {
+          name: "wordpress",
+          iconifyClass: "logos-wordpress",
+        },
+      ],
+    },
+    
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
